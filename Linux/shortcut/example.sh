378a640b8b6417bafaf8a63fbc1953e1eb1353ce
ChenYing Kuo <evshary@gmail.com>
2020-01-22 14:48:11 +0800

    Update example.sh

--- Linux/shortcut/example.sh
+++ Linux/shortcut/example.sh
@@ -2,7 +2,7 @@
 gnome-terminal -t "Terminal example 1" -e top &
 gnome-terminal -t "Terminal example 2" &
 # If you want to source rc file before doing something
-#gnome-terminal -t "Terminal with source file" -e 'bash -c "source ~/.rc_file; ls"'
+#gnome-terminal -t "Terminal with source file" -e 'bash -c "source ~/.rc_file; ls"' &
 # However, note that you should not source .bashrc directly, since there is interaction detection in bashrc.
 # If it's not interactive terminal, it doesn't allow you to source bashrc.
 exit 0
