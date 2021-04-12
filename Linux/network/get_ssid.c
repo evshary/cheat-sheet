21415717f02e4d4f4a064e698c5a141bbc4283d8
ChenYing Kuo <evshary@gmail.com>
2021-04-12 10:37:54 +0800

    Adjust coding style.

--- Linux/network/get_ssid.c
+++ Linux/network/get_ssid.c
@@ -12,15 +12,15 @@ int main(void)
     int sock_fd;
     struct iwreq wreq;
 
-	memset(&wreq, 0, sizeof(struct iwreq));
-	sprintf(wreq.ifr_name, WIRELESS_INTERFACE);
+    memset(&wreq, 0, sizeof(struct iwreq));
+    sprintf(wreq.ifr_name, WIRELESS_INTERFACE);
 
     if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         printf("Unable to open socket\n");
         return -1;
     }
-	wreq.u.essid.pointer = malloc(IW_ESSID_MAX_SIZE+1);
-	wreq.u.essid.length = IW_ESSID_MAX_SIZE;
+    wreq.u.essid.pointer = malloc(IW_ESSID_MAX_SIZE+1);
+    wreq.u.essid.length = IW_ESSID_MAX_SIZE;
     if (ioctl(sock_fd, SIOCGIWESSID, &wreq)) {
         printf("Unable to get ESSID\n");
         return -1;
