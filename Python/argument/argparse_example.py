80f21acb8fd5d83ae17650568a3feeaca4042719
ChenYing Kuo <evshary@gmail.com>
2021-10-13 11:20:42 +0800

    Support subcmd for argparse.

--- Python/argument/argparse_example.py
+++ Python/argument/argparse_example.py
@@ -4,18 +4,60 @@ import argparse
 
 VERSION_STR="0.9.0"
 
-def main():
+# usage: argparse_example.py [-h] [-v] cmd [arg [arg ...]]
+# 
+# Argument Test Example
+# 
+# positional arguments:
+#   cmd            My command
+#   arg            Argument for each command
+# 
+# optional arguments:
+#   -h, --help     show this help message and exit
+#   -v, --version  show program's version number and exit
+def _simple_parser():
     parser = argparse.ArgumentParser(description="Argument Test Example")
     parser.add_argument('command', metavar='cmd', nargs=1,
                         help="My command")
     parser.add_argument('argument', metavar='arg', nargs='*',
                         help="Argument for each command")
     parser.add_argument('-v', '--version', action='version', version=VERSION_STR)
-    # Parse argument
+
     args = parser.parse_args()
     # Print the output of parsing arugment
     print(args.command)
     print(args.argument)
+    return args
+
+# 
+def _subcmd_parser(): 
+    parser = argparse.ArgumentParser(description="Argument Test Example")
+    parser.add_argument('-v', '--version', action='version', version=VERSION_STR)
+    subcmd = parser.add_subparsers(dest='subcmd', metavar='cmd', required=True, help="The command you want")
+
+    # list
+    list_parser = subcmd.add_parser('list', help="List all items")
+    list_parser.add_argument('path', metavar='PATH', help="Where the path")
+
+    # create
+    create_parser = subcmd.add_parser('create', help="Create Parser")
+    create_parser.add_argument('project', metavar='PROJ', help="Your project name")
+    create_parser.add_argument('-n', type=int, help='project number')
+
+    args = parser.parse_args()
+    if args.subcmd == "list":
+        print("list: "+args.path)
+    elif args.subcmd == "create":
+        print("create: "+args.project)
+        print("        n=%d" % args.n)
+    return args
+
+def main():
+    # Parse argument
+    #args = _simple_parser()
+    args = _subcmd_parser()
+    # Do what you want to do
+
 
 if __name__ == '__main__':
     main()
