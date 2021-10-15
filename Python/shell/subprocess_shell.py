3fd1e45d572457fbdd713fa807a001ce7325672d
ChenYing Kuo <evshary@gmail.com>
2021-10-15 15:29:25 +0800

    Rename the returncode.

--- Python/shell/subprocess_shell.py
+++ Python/shell/subprocess_shell.py
@@ -4,12 +4,12 @@ import subprocess
 
 # The output will show in stdout
 ## Only one command
-file_lists = subprocess.run("ls")
+process_runner = subprocess.run("ls")
 ## To get the return code
-print("The `ls` return code is %d" % file_lists.returncode)
+print("The `ls` return code is %d" % process_runner.returncode)
 
 
 ## Command with argments
-file_lists = subprocess.run("ls -al", shell=True)
+process_runner = subprocess.run("ls -al", shell=True)
 ## To get the return code
-print("The `ls -al` return code is %d" % file_lists.returncode)
+print("The `ls -al` return code is %d" % process_runner.returncode)
