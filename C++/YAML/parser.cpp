5c5cac85b3dd5c3dab3e689d331b623cca59ab08
ChenYing Kuo <evshary@gmail.com>
2021-10-22 13:28:45 +0800

    Able to output YAML file.

--- C++/YAML/parser.cpp
+++ C++/YAML/parser.cpp
@@ -6,9 +6,14 @@ int main() {
 
     yaml_data = YAML::LoadFile("test.yaml");
 
+    // print value
+    std::cout << "Example: try to print value" << std::endl;
     std::string mytopic = yaml_data["mytopic"].as<std::string>();
     std::cout << "mytopic: " << mytopic << std::endl;
 
+    // print list
+    std::cout << std::endl;
+    std::cout << "Example: try to print list" << std::endl;
     std::cout << "Print directly" << std::endl;
     std::cout << yaml_data["mylist"] << std::endl;
 
@@ -16,6 +21,26 @@ int main() {
     for (YAML::const_iterator it = yaml_data["mylist"].begin(); it != yaml_data["mylist"].end(); ++it) {
         std::cout << it->first << ": " << it->second << std::endl;
     }
+
+    // The difference of using as<std::string>()
+    std::cout << std::endl;
+    std::cout << "Example: show the difference of using as<std::string>()" << std::endl;
+    std::cout << "print directly => item1: " << yaml_data["mylist"]["item 1"] << std::endl;
+    std::cout << "print as<std::string>() => item1: " << yaml_data["mylist"]["item 1"].as<std::string>() << std::endl;
+
+    // Output yaml file
+    std::cout << std::endl;
+    std::cout << "Output YAML string" << std::endl;
+    YAML::Emitter out;
+    out << YAML::BeginMap;
+    out << YAML::Key << "name";
+    out << YAML::Value << "evshary";
+    out << YAML::Key << "comment";
+    out << YAML::Value << "Hello\nworld";
+    out << YAML::EndMap;
+    std::cout << "----------------------" << std::endl;
+    std::cout << out.c_str() << std::endl;
+    std::cout << "----------------------" << std::endl;
 
     return 0;
 }
